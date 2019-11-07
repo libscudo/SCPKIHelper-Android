54b8ae1bc900b1a057a540dbc0820dd38c78d19d
Ezequiel Aceto <eaceto@afip.gob.ar>
2019-11-07 10:00:16 -0300

    Implement retrieve PublicKey

--- libSCPKIHelper/src/main/java/ar/com/ezequielaceto/mobile/android/libraries/pki/SCPKIHelper.java
+++ libSCPKIHelper/src/main/java/ar/com/ezequielaceto/mobile/android/libraries/pki/SCPKIHelper.java
@@ -4,13 +4,23 @@ import android.content.Context;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PublicKey;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.Enumeration;
+
+import javax.crypto.SecretKey;
 
 import static android.security.keystore.KeyProperties.PURPOSE_ENCRYPT;
 import static android.security.keystore.KeyProperties.PURPOSE_DECRYPT;
@@ -39,7 +49,7 @@ public final class SCPKIHelper {
         try {
             KeyPairGenerator kpg = KeyPairGenerator.getInstance(specs.getKeyType(), specs.getProvider());
             kpg.initialize(new KeyGenParameterSpec.Builder(
-                    serviceName + "." + identifier,
+                    aliasFor(identifier),
                     PURPOSE_SIGN | PURPOSE_VERIFY | PURPOSE_DECRYPT | PURPOSE_ENCRYPT)
                     .setDigests(specs.getDigest(), specs.getDigest())
                     .setEncryptionPaddings(specs.getEncryptionPadding(), specs.getEncryptionPadding())
@@ -56,7 +66,7 @@ public final class SCPKIHelper {
             if (causeMessage.contains("secure") && causeMessage.contains("lock") && causeMessage.contains("screen")) {
                 throw new AuthenticationRequiredException(e);
             }
-            throw new AlgorithmException(e.getMessage());
+            throw new AlgorithmException(e);
         } catch (NoSuchProviderException e) {
             throw new KeyStoreException(e);
         } catch (Exception e) {
@@ -64,19 +74,53 @@ public final class SCPKIHelper {
         }
     }
 
-    public PublicKey getPublicKey(SCPKIKeySpec specs, String identifier) {
-        return null;
+    public PublicKey getPublicKey(SCPKIKeySpec specs, String identifier) throws KeyStoreException, AlgorithmException, KeyNotFoundForIdentifier {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(specs.getProvider());
+            keyStore.load(null);
+
+            Certificate certificate = keyStore.getCertificate(aliasFor(identifier));
+            if (certificate != null) {
+                return certificate.getPublicKey();
+            }
+        } catch (CertificateException | IOException | NoSuchAlgorithmException | java.security.KeyStoreException e) {
+            if (e instanceof NoSuchAlgorithmException) {
+                throw new AlgorithmException(e);
+            }
+            throw new KeyStoreException(e);
+        }
+        throw new KeyNotFoundForIdentifier();
+    }
+
+    @NotNull
+    private String aliasFor(String identifier) {
+        return serviceName + "." + identifier;
     }
 
 
     public class AlgorithmException extends Exception {
-        AlgorithmException(String message) {
-            super(message);
+        AlgorithmException(Throwable t) {
+            super(t);
         }
     }
 
     public class AuthenticationRequiredException extends Exception {
         AuthenticationRequiredException(Throwable t) {
+            super(t);
+        }
+    }
+
+    public class KeyStoreException extends Exception {
+        KeyStoreException(Throwable t) {
+            super(t);
+        }
+    }
+
+    public class KeyNotFoundForIdentifier extends Exception {
+        KeyNotFoundForIdentifier() {
+            super();
+        }
+        KeyNotFoundForIdentifier(Throwable t) {
             super(t);
         }
     }
