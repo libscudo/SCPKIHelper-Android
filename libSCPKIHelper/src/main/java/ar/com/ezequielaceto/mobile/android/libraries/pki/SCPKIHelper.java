98b99f425a1bcf0893e2ed0bc3d9c826585eebca
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2019-11-07 07:33:12 -0300

    Implement custom exceptions

--- libSCPKIHelper/src/main/java/ar/com/ezequielaceto/mobile/android/libraries/pki/SCPKIHelper.java
+++ libSCPKIHelper/src/main/java/ar/com/ezequielaceto/mobile/android/libraries/pki/SCPKIHelper.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
 public final class SCPKIHelper {
 
@@ -25,19 +29,45 @@ public final class SCPKIHelper {
     }
 
     public KeyPair generateKeyPair(SCPKIKeySpec specs, String identifier) throws Exception {
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance(specs.getKeyType(), specs.getProvider());
-        kpg.initialize(new KeyGenParameterSpec.Builder(
-                serviceName + "." + identifier,
-                KeyProperties.PURPOSE_SIGN | KeyProperties.PURPOSE_VERIFY)
-                .setDigests(specs.getDigest(), specs.getDigest())
-                .setEncryptionPaddings(specs.getPadding(), specs.getPadding())
-                .setUserAuthenticationRequired(specs.requireUserAuthentication())
-                .setUserAuthenticationValidityDurationSeconds(specs.getReuseAuthenticationDuration())
-                .build());
 
-        KeyPair keyPair = kpg.generateKeyPair();
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance(specs.getKeyType(), specs.getProvider());
+            kpg.initialize(new KeyGenParameterSpec.Builder(
+                    serviceName + "." + identifier,
+                    KeyProperties.PURPOSE_SIGN | KeyProperties.PURPOSE_VERIFY)
+                    .setDigests(specs.getDigest(), specs.getDigest())
+                    .setEncryptionPaddings(specs.getPadding(), specs.getPadding())
+                    .setUserAuthenticationRequired(specs.requireUserAuthentication())
+                    .setUserAuthenticationValidityDurationSeconds(specs.getReuseAuthenticationDuration())
+                    .build());
 
-        return keyPair;
+            KeyPair keyPair = kpg.generateKeyPair();
+
+            return keyPair;
+        } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException e) {
+            String causeMessage = e.getCause().getMessage().toLowerCase();
+            if (causeMessage.contains("secure") && causeMessage.contains("lock") && causeMessage.contains("screen")) {
+                throw new AuthenticationRequiredException(e);
+            }
+            throw new AlgorithmException(e.getMessage());
+        } catch (NoSuchProviderException e) {
+            throw new KeyStoreException(e);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    public class AlgorithmException extends Exception {
+        AlgorithmException(String message) {
+            super(message);
+        }
+    }
+
+    public class AuthenticationRequiredException extends Exception {
+        AuthenticationRequiredException(Throwable t) {
+            super(t);
+        }
     }
 
 }
